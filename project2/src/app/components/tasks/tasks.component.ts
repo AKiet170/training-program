784b02864be998ddae3d075e61d008234cb4cc95
Kit <phananhkiet1710@gmail.com>
2025-11-17 09:43:47 +0700

    tasks done

--- project2/src/app/components/tasks/tasks.component.ts
+++ project2/src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,27 @@
+
 import { Component } from '@angular/core';
+
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [NzListModule, NzTypographyModule],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
 export class TasksComponent {
+  data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.'
+  ];
 
+  constructor(public msg: NzMessageService) {}
 }
+
+
--- project2/src/app/components/weather/weather.component.ts
+++ project2/src/app/components/weather/weather.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './weather.component.scss'
 })
 export class WeatherComponent {
-
+  city='TP.HCM';
+  temp=35;
+  condition='overcast clouds';
+  humidity=70;
+  windSpeed=10;
+  cloudCover=50
 }
